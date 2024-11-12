274d0dd9e4e374687d1c5e7ed3256d51b2992902
aminelli <56305052+aminelli@users.noreply.github.com>
2024-11-12 14:22:00 +0100

    upd

---
+++ ES001/script.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    const message = 'Questo è un messaggio di debug!';
+    // debugger; // Questo è un breakpoint per il debug
+    console.log(message);
+    alert(message);
+}
